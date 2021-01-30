0dd0add512ede6d452e7b7a025249e5081807e19
Medet Tleukabiluly <mt@codemotion.eu>
2021-01-30 17:23:10 +0600

    complex table fix

--- src/components/ComplexTable/index.tsx
+++ src/components/ComplexTable/index.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 import { DataTable, Menu } from 'react-native-paper'
 
-type SortDirection = 'ascending' | 'descending' | undefined
-type Sort = {
-  field: string
-  direction: SortDirection
-}
 type Row<T> = {
   label: string
   value: keyof T
@@ -26,7 +22,7 @@ type State<K> = {
   ordering?: keyof typeof Order
   column2Show: keyof K
 }
-export default function Screen <T> ({
+export default function Screen<T> ({
   data,
   first,
   middle,
@@ -81,48 +77,76 @@ export default function Screen <T> ({
   return (
     <DataTable>
       <DataTable.Header>
-        <DataTable.Title sortDirection={state.orderedColumn === 1 ? state.ordering : undefined} onPress={() => {
-          setState({
-            ...state,
-            orderedColumn: 1,
-            ordering: state.orderedColumn === 1 ? state.ordering === 'ascending' ? 'descending' : 'ascending' : 'descending'
-          })
-        }}>{first.label}</DataTable.Title>
+        <DataTable.Title
+          sortDirection={state.orderedColumn === 1 ? state.ordering : undefined}
+          onPress={() => {
+            setState({
+              ...state,
+              orderedColumn: 1,
+              ordering:
+                state.orderedColumn === 1
+                  ? state.ordering === 'ascending'
+                      ? 'descending'
+                      : 'ascending'
+                  : 'descending'
+            })
+          }}
+        >
+          {first.label}
+        </DataTable.Title>
+        <DataTable.Title onPress={openMenu} numeric>
+          {middle.find((v) => v.value === state.column2Show)?.label}
+        </DataTable.Title>
         <Menu
           visible={visible}
           onDismiss={closeMenu}
-          anchor={<DataTable.Title onPress={openMenu} numeric>{middle.find(v => v.value === state.column2Show)?.label}</DataTable.Title>}>
-          {
-            middle.map((v, i) => (
-              <Menu.Item icon={v.value === state.column2Show ? 'checkbox-marked' : 'checkbox-blank-outline'} key={i} onPress={() => handleMenuSelect(v)} title={v.label} />
-            ))
-          }
+          anchor={<View style={s.menuHack} />}
+        >
+          {middle.map((v, i) => (
+            <Menu.Item
+              icon={
+                v.value === state.column2Show
+                  ? 'checkbox-marked'
+                  : 'checkbox-blank-outline'
+              }
+              key={i}
+              onPress={() => handleMenuSelect(v)}
+              title={v.label}
+            />
+          ))}
         </Menu>
-        <DataTable.Title sortDirection={state.orderedColumn === 3 ? state.ordering : undefined} numeric onPress={() => {
-          setState({
-            ...state,
-            orderedColumn: 3,
-            ordering: state.orderedColumn === 3 ? state.ordering === 'ascending' ? 'descending' : 'ascending' : 'descending'
-          })
-        }}>{last.label}</DataTable.Title>
+        <DataTable.Title
+          sortDirection={state.orderedColumn === 3 ? state.ordering : undefined}
+          numeric
+          onPress={() => {
+            setState({
+              ...state,
+              orderedColumn: 3,
+              ordering:
+                state.orderedColumn === 3
+                  ? state.ordering === 'ascending'
+                      ? 'descending'
+                      : 'ascending'
+                  : 'descending'
+            })
+          }}
+        >
+          {last.label}
+        </DataTable.Title>
       </DataTable.Header>
 
-      {
-        sortedData.map((item, index) => (
-          <DataTable.Row key={index} onPress={() => onPress && onPress(item)}>
-            <DataTable.Cell>{item[first.value]}</DataTable.Cell>
-            <DataTable.Cell numeric>
-              {item[state.column2Show]}
-            </DataTable.Cell>
-            <DataTable.Cell numeric>{item[last.value]}</DataTable.Cell>
-          </DataTable.Row>
-        ))
-      }
+      {sortedData.map((item, index) => (
+        <DataTable.Row key={index} onPress={() => onPress && onPress(item)}>
+          <DataTable.Cell>{item[first.value]}</DataTable.Cell>
+          <DataTable.Cell numeric>{item[state.column2Show]}</DataTable.Cell>
+          <DataTable.Cell numeric>{item[last.value]}</DataTable.Cell>
+        </DataTable.Row>
+      ))}
 
       <DataTable.Pagination
         page={1}
         numberOfPages={3}
-        onPageChange={page => {
+        onPageChange={(page) => {
           console.log(page)
         }}
         label='1-2 of 6'
@@ -130,3 +154,7 @@ export default function Screen <T> ({
     </DataTable>
   )
 }
+
+const s = StyleSheet.create({
+  menuHack: { width: 1, height: 1 }
+})
